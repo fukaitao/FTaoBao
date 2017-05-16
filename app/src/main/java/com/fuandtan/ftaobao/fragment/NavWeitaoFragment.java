c4ab973fda276e8aaf74042516095147f68017c4
ZZZ <254359498@qq.com>
2017-05-16 21:08:41 +0800

    模块：weitao 修改*）标题栏隐现1.0修改版

--- app/src/main/java/com/fuandtan/ftaobao/fragment/NavWeitaoFragment.java
+++ app/src/main/java/com/fuandtan/ftaobao/fragment/NavWeitaoFragment.java
@@ -1,6 +1,7 @@
 package com.fuandtan.ftaobao.fragment;
 
 import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -10,11 +11,15 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,18 +31,40 @@ import java.util.List;
 
 
 public class NavWeitaoFragment extends Fragment implements Animator.AnimatorListener {
+    // 总布局
     private View weitaoView;
-    private View tf_action_bar;
+
+    // 标题栏布局
+    private LinearLayout tf_app_bar_layout;
+    private LinearLayout tf_action_bar_layout;
+    private RelativeLayout tf_action_bar;
     private LinearLayout tf_tab_bar;
+    // tab_bar图标
+    private ImageView iv_weitao_dynamic;
+    private ImageView iv_weitao_new;
+    private ImageView iv_weitao_direct_seeding;
+    private ImageView iv_weitao_hot_topic;
+    //tab_bar文字
+    private TextView tv_weitao_dynamic;
+    private TextView tv_weitao_new;
+    private TextView tv_weitao_direct_seeding;
+    private TextView tv_weitao_hot_topic;
+
+    // 数据内容布局
     private RecyclerView recyclerView;
-    private RecyclerViewAdapter recyclerViewAdapter;
+    private RecyclerViewAdapter recyclerViewAdapter;// 数据适配器
+    private ListView tf_tab_page;// 数据页面
+    private List<String> items;// 数据项
+    private int mOrientation;// 数据走向
 
-    private ListView tf_tab_page;
-    private List<String> items;
-
-    private int mOrientation;
-    //状态栏布局
+    // 状态栏布局
     private LinearLayout statusBar;
+
+    // 标题栏
+    private boolean toTop;
+    private boolean isActionbarHide;
+    private boolean mIsAnim;
+    private static int TIME_ANIMATION = 800;
 
     /**
      * fragment 生命周期：
@@ -61,23 +88,48 @@ public class NavWeitaoFragment extends Fragment implements Animator.AnimatorList
 
     private void initData() {
         items = new ArrayList<String>();
-        for (int i = 1; i < 100; i++) {
-            items.add("" + i);
+        String str;
+        for (int i = 1; i < 666; i++) {
+            str = "000" + i;
+            items.add(str.substring(str.length() - 3, str.length()));
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // 总布局实例化
         weitaoView = inflater.inflate(R.layout.nav_weitao_fragment, container, false);
         initView();//初始化布局
         return weitaoView;
     }
 
     private void initView() {
-        tf_action_bar = weitaoView.findViewById(R.id.tf_action_bar);
+        tf_app_bar_layout = (LinearLayout) weitaoView.findViewById(R.id.tf_app_bar_layout);
+
+        //得到状态栏布局对象
+        statusBar = (LinearLayout) weitaoView.findViewById(R.id.ll_normal_status_bar);
+        //动态de设置状态高度为得到的状态栏高度
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) statusBar.getLayoutParams();
+        params.height = Utils.getStatusBarHeight(getActivity());
+        statusBar.setLayoutParams(params);
+
+        // 标题栏布局实例化
+        tf_action_bar_layout = (LinearLayout) weitaoView.findViewById(R.id.tf_action_bar_layout);
+        tf_action_bar = (RelativeLayout) weitaoView.findViewById(R.id.tf_action_bar);
         tf_tab_bar = (LinearLayout) weitaoView.findViewById(R.id.tf_tab_bar);
+        // tab_bar图标实例化
+        iv_weitao_dynamic = (ImageView) weitaoView.findViewById(R.id.iv_weitao_dynamic);
+        iv_weitao_new = (ImageView) weitaoView.findViewById(R.id.iv_weitao_new);
+        iv_weitao_direct_seeding = (ImageView) weitaoView.findViewById(R.id.iv_weitao_direct_seeding);
+        iv_weitao_hot_topic = (ImageView) weitaoView.findViewById(R.id.iv_weitao_hot_topic);
+        //tab_bar文字实例化
+        tv_weitao_dynamic = (TextView) weitaoView.findViewById(R.id.tv_weitao_dynamic);
+        tv_weitao_new = (TextView) weitaoView.findViewById(R.id.tv_weitao_new);
+        tv_weitao_direct_seeding = (TextView) weitaoView.findViewById(R.id.tv_weitao_direct_seeding);
+        tv_weitao_hot_topic = (TextView) weitaoView.findViewById(R.id.tv_weitao_hot_topic);
+
+        // 数据内容布局实例化
         recyclerView = (RecyclerView) weitaoView.findViewById(R.id.tf_tab_page);
     }
 
@@ -89,12 +141,80 @@ public class NavWeitaoFragment extends Fragment implements Animator.AnimatorList
         recyclerView.setAdapter(recyclerViewAdapter = new RecyclerViewAdapter());
         //设置RecyclerView的分割线
         recyclerView.addItemDecoration(new RecyclerViewItem(getActivity(), LinearLayoutManager.VERTICAL));
-        // 得到状态栏布局对象
-        statusBar = (LinearLayout) weitaoView.findViewById(R.id.ll_normal_status_bar);
-        // 动态设置状态高度为得到的状态栏高度
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) statusBar.getLayoutParams();
-        params.height = Utils.getStatusBarHeight(getActivity());
-        statusBar.setLayoutParams(params);
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.d("txxz", "onScrollStateChanged()--newState=" + newState);
+                LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                // 当不滚动时
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    //获取最后一个完全显示的ItemPosition
+                    int lastVisibleItem = manager.findLastCompletelyVisibleItemPosition();
+                    int totalItemCount = manager.getItemCount();
+
+                    // 判断是否滚动到底部，并且是向下滚动
+                    if (lastVisibleItem == (totalItemCount - 1) && !toTop) {
+                        //加载更多功能的代码
+                        Toast.makeText(getActivity(), "客官别扯了，我们是有底线的", Toast.LENGTH_SHORT).show();
+                    }
+                } else if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    Log.d("txxz", "onScrollStateChanged()--toTop:" + toTop + "  isActionbarHide:" + isActionbarHide);
+                    if (!toTop && !isActionbarHide) {// 向下滑，隐藏标题栏
+                        Log.d("txxz", "onScrollStateChanged()--SCROLL_STATE_DRAGGING--向下滑");
+                        ObjectAnimator animator1 = ObjectAnimator.ofFloat(tf_action_bar, "alpha", 1.0f, 0.0f);
+                        ObjectAnimator animator2 = ObjectAnimator.ofFloat(tf_tab_bar, "translationY", 0.0f, -tf_action_bar.getHeight());
+                        initAnimator(animator1);
+                        initAnimator(animator2);
+                        isActionbarHide = !isActionbarHide;
+                    } else if (toTop && isActionbarHide) {// 向上滑，显示标题栏
+                        Log.d("txxz", "onScrollStateChanged()--SCROLL_STATE_DRAGGING--向上滑");
+                        ObjectAnimator animator1 = ObjectAnimator.ofFloat(tf_action_bar, "alpha", 0.0f, 1.0f);
+                        ObjectAnimator animator2 = ObjectAnimator.ofFloat(tf_tab_bar, "translationY", -tf_action_bar.getHeight(), 0.0f);
+                        initAnimator(animator1);
+                        initAnimator(animator2);
+                        isActionbarHide = !isActionbarHide;
+                    } else if (!toTop && isActionbarHide) {
+                        Toast.makeText(getActivity(), "上滑后再向下滑", Toast.LENGTH_SHORT).show();
+                    } else if (toTop && !isActionbarHide) {
+                        Toast.makeText(getActivity(), "下滑后再向上滑", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+//                Toast.makeText(getActivity(), "dy:" + dy, Toast.LENGTH_SHORT).show();
+                Log.d("txxz", "onScrolled()--toTop:" + toTop + "  isActionbarHide:" + isActionbarHide
+                        + "  dy:" + dy);
+                if (dy > 0) {// 向下滑，隐藏标题栏
+                    toTop = false;
+                } else if (dy < 0) {// 向上滑，显示标题栏
+                    toTop = true;
+                }
+            }
+        });
+    }
+
+    private void initAnimator(Animator animator) {
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(TIME_ANIMATION);
+        animator.start();
+        animator.addListener(NavWeitaoFragment.this);
+    }
+
+    /**
+     * 设置标题栏变化高度
+     *
+     * @param page
+     */
+    public void setMarginTop(int page) {
+        RelativeLayout.LayoutParams layoutParam = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        layoutParam.setMargins(0, page, 0, 0);
+        recyclerView.setLayoutParams(layoutParam);
+        recyclerView.invalidate();
     }
 
     /**
@@ -195,7 +315,7 @@ public class NavWeitaoFragment extends Fragment implements Animator.AnimatorList
             holder.tv_item_weitao.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getActivity(), holder.tv_item_weitao.getText(), Toast.LENGTH_LONG);
+                    Toast.makeText(getActivity(), "click ：" + holder.tv_item_weitao.getText(), Toast.LENGTH_LONG).show();
                 }
             });
         }
@@ -205,7 +325,7 @@ public class NavWeitaoFragment extends Fragment implements Animator.AnimatorList
             return items.size();
         }
     }
-
+    
     /**
      * itemView持有类
      */
@@ -225,12 +345,18 @@ public class NavWeitaoFragment extends Fragment implements Animator.AnimatorList
      */
     @Override
     public void onAnimationStart(Animator animation) {
-
+        Log.d("txxz", "onAnimationStart()--toTop:" + toTop + "  isActionbarHide:" + isActionbarHide);
     }
 
     @Override
     public void onAnimationEnd(Animator animation) {
-
+        Log.d("txxz", "onAnimationEnd()--toTop:" + toTop + "  isActionbarHide:" + isActionbarHide
+                + "  tf_app_bar_layout.getHeight():" + tf_app_bar_layout.getHeight() + "  tf_action_bar.getHeight():" + tf_action_bar.getHeight());
+        if (toTop)
+            setMarginTop(tf_app_bar_layout.getHeight());
+        else
+            setMarginTop(tf_app_bar_layout.getHeight() - tf_action_bar.getHeight());
+        mIsAnim = false;
     }
 
     @Override
